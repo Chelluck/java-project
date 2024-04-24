53a977b1427e9e5a3b2d562249f98ae945f0a3ba
Cheluck <134635093+Chelluck@users.noreply.github.com>
2024-04-24 22:55:19 +0500

    L6 Resource system

--- L6 Resource system/src/main/java/main/Main.java
+++ L6 Resource system/src/main/java/main/Main.java
@@ -1,37 +1,58 @@
 package main;
 
 
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import resourceServer.ResourceServer;
+import resourceServer.ResourceServerController;
+import resourceServer.ResourceServerControllerMBean;
+import resourceServer.ResourceServerI;
+import servlets.ResourcePageServlet;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
 
-/**
- * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
- */
+
+
 public class Main {
+    static final Logger logger = LogManager.getLogger(Main.class.getName());
+
     public static void main(String[] args) throws Exception {
-        RandomAccessFile aFile = new RandomAccessFile("data/data.txt", "rw");
-        FileChannel inChannel = aFile.getChannel();
 
-        ByteBuffer buf = ByteBuffer.allocate(64);
+        int port = 8080;
+        logger.info("Starting at http://127.0.0.1:" + port);
 
-        int bytesRead = inChannel.read(buf);
-        while (bytesRead != -1) {
+        ResourceServer resourceServer = new ResourceServerI();
 
-            System.out.println("Read " + bytesRead);
-            buf.flip();
+        ResourceServerControllerMBean serverStats = new ResourceServerController(resourceServer);
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName subject = new ObjectName("Admin:type=ResourceServerController");
+        mbs.registerMBean(serverStats, subject);
 
-            while (buf.hasRemaining()) {
-                System.out.print((char) buf.get());
-            }
-            System.out.print("\n");
-            buf.clear();
-            bytesRead = inChannel.read(buf);
-        }
-        aFile.close();
+
+        Server server = new Server(port);
+
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        contextHandler.addServlet(new ServletHolder(new ResourcePageServlet(resourceServer)),
+                  ResourcePageServlet.PAGE_URL);
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setResourceBase("public_html");
+
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resourceHandler, contextHandler});
+        server.setHandler(handlers);
+
+        server.start();
+        logger.info("Server started");
+        server.join();
     }
 }
